b61c62a3230975efe86748aafc1d6c7ab73009a3
Robert Nelson <robertcnelson@gmail.com>
2025-05-30 08:18:19 -0500

    bbb.io-kernel (1.20250530.0)
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- bbb.io-kernel/update.sh
+++ bbb.io-kernel/update.sh
@@ -229,7 +229,9 @@ do_some_bone () {
 	msg="6.9-bone"  ; var="omap-psp" ; ver="V69X"  ; current_kernel ; generate_kernel_mainline_bone
 	msg="6.10-bone" ; var="omap-psp" ; ver="V610X" ; current_kernel ; generate_kernel_mainline_bone
 	msg="6.11-bone" ; var="omap-psp" ; ver="V611X" ; current_kernel ; generate_kernel_mainline_bone
-	msg="6.12-bone" ; var="omap-psp" ; ver="V612X" ; current_kernel ; generate_kernel_mainline_bone
+
+	msg="6.12-bone"    ; var="omap-psp" ; ver="V612X" ; current_kernel ; generate_kernel_mainline_bone
+	msg="6.12-bone-rt" ; var="bone-rt"  ; ver="V612X" ; current_kernel ; generate_kernel_mainline_bone
 
 	msg="6.13-bone" ; var="omap-psp" ; ver="V613X" ; current_kernel ; generate_kernel_mainline_bone
 	msg="6.14-bone" ; var="omap-psp" ; ver="V614X" ; current_kernel ; generate_kernel_mainline_bone
--- bbb.io-kernel/version.sh
+++ bbb.io-kernel/version.sh
@@ -2,7 +2,7 @@
 
 package_name="bbb.io-kernel"
 debian_pkg_name="${package_name}"
-package_version="1.20250529.0"
+package_version="1.20250530.0"
 package_source=""
 src_dir=""
 
@@ -17,7 +17,7 @@ debian_patch=""
 
 clear_changelog="enable"
 
-bullseye_version="~bullseye+20250529"
-bookworm_version="~bookworm+20250529"
-trixie_version="~trixie+20250529"
-noble_version="~noble+20250529"
+bullseye_version="~bullseye+20250530"
+bookworm_version="~bookworm+20250530"
+trixie_version="~trixie+20250530"
+noble_version="~noble+20250530"
