8d6f512939c2e43ffac965184815a401e464745c
Robert Nelson <robertcnelson@gmail.com>
2025-06-27 13:09:33 -0500

    bb-u-boot-am335x-evm (2022.04.20240115.5)
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- bb-u-boot-am335x-evm-debian-11/version.sh
+++ bb-u-boot-am335x-evm-debian-11/version.sh
@@ -4,7 +4,7 @@
 
 package_name="bb-u-boot-am335x-evm"
 debian_pkg_name="${package_name}"
-package_version="2022.04.20240115.4"
+package_version="2022.04.20240115.5"
 package_source=""
 src_dir=""
 
@@ -21,7 +21,7 @@ clear_changelog="enable"
 
 focal_version="~focal+20240115"
 jammy_version="~jammy+20250217"
-bullseye_version="~bullseye+20250626"
-bookworm_version="~bookworm+20250626"
-trixie_version="~trixie+20250626"
-noble_version="~noble+20250626"
+bullseye_version="~bullseye+20250627"
+bookworm_version="~bookworm+20250627"
+trixie_version="~trixie+20250627"
+noble_version="~noble+20250627"
