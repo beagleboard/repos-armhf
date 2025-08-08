ab86776633c9172c57be75303f227cb1c722dd0c
Robert Nelson <robertcnelson@gmail.com>
2025-08-08 13:39:33 -0500

    bb-customizations (1.20250808.0)
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- bb-customizations/version.sh
+++ bb-customizations/version.sh
@@ -4,7 +4,7 @@
 
 package_name="bb-customizations"
 debian_pkg_name="${package_name}"
-package_version="1.20240119.0"
+package_version="1.20250808.0"
 package_source=""
 src_dir=""
 
@@ -19,10 +19,7 @@ debian_patch=""
 
 clear_changelog="enable"
 
-buster_version="~buster+20240119"
-bullseye_version="~bullseye+20240119"
-bookworm_version="~bookworm+20240119"
-trixie_version="~trixie+20240119"
-focal_version="~focal+20240119"
-jammy_version="~jammy+20240119"
-noble_version="~noble+20240119"
+bullseye_version="~bullseye+20250808"
+bookworm_version="~bookworm+20250808"
+trixie_version="~trixie+20250808"
+noble_version="~noble+20250808"
