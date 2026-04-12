70a5b55300a1320c57f0e22702330c5f63fd4f93
Robert Nelson <robertcnelson@gmail.com>
2026-04-12 11:44:13 -0500

    add gitlab-runner to repo
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

---
+++ gitlab-runner-trixie/add_pkg.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+build="18.10.1-1"
+
+base="/var/www/html/repos/"
+outgoing="/var/www/html/farm/outgoing"
+
+run () {
+	apt-get download gitlab-runner:${deb_arch}=${build} || true
+	apt-get download gitlab-runner-helper-images=${build} || true
+	sync
+	if [ -f ./gitlab-runner_${build}_${deb_arch}.deb ] ; then
+		if [ -f ./gitlab-runner-helper-images_${build}_all.deb ] ; then
+			reprepro -b ${repo} -C main includedeb ${suite} ./gitlab-runner_${build}_${deb_arch}.deb
+			reprepro -b ${repo} -C main includedeb ${suite} ./gitlab-runner-helper-images_${build}_all.deb
+		fi
+	fi
+}
+
+runner () {
+	repo="${base}${dist}/"
+	echo ${suite}
+	run
+}
+
+start_run () {
+	deb_arch="armhf"
+
+	dist="debian-trixie-armhf"
+	suite="trixie" ; runner
+}
+
+if [ ! -f /tmp/cron.lock ] ; then
+	start_run
+else
+	echo "reprepro running"
+fi
+#
