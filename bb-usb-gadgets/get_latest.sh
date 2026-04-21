39da3019bb65bac16b19808a7e1f24ffeba61eb2
Robert Nelson <robertcnelson@gmail.com>
2026-04-21 15:53:47 -0500

    bb-usb-gadgets (1.20260421.7)
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- bb-usb-gadgets/get_latest.sh
+++ bb-usb-gadgets/get_latest.sh
@@ -9,8 +9,10 @@ dl_src () {
 }
 
 copy_all () {
+	cp -v ./src${wdir}${wfile} ./suite/bookworm/debian/
 	cp -v ./src${wdir}${wfile} ./suite/trixie/debian/
 	cp -v ./src${wdir}${wfile} ./suite/forky/debian/
+	cp -v ./src${wdir}${wfile} ./suite/noble/debian/
 }
 
 dl_src
