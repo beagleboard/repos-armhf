2f713e6f5925e57a11043f3785f1fb0391004685
Robert Nelson <robertcnelson@gmail.com>
2026-04-22 11:53:20 -0500

    generic-sys-mods (1.20260422.0) sync outside repo scripts
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- generic-sys-mods/get_latest.sh
+++ generic-sys-mods/get_latest.sh
@@ -8,6 +8,15 @@ dl_src () {
 	git clone git@github.com:beagleboard/bbbio-set-sysconf.git ./src/
 }
 
+dl_usr_src () {
+	if [ -d ./src/ ] ; then
+		rm -rf ./src/
+	fi
+
+	#Mirror: https://github.com/beagleboard/customizations
+	git clone ssh://git@forgejo.gfnd.rcn-ee.org:222/BeagleBoard.org/usr-customizations.git ./src/
+}
+
 copy_all () {
 	cp -v ./src/${wfile} ./suite/bookworm/debian/
 	cp -v ./src/${wfile} ./suite/noble/debian/
@@ -18,3 +27,11 @@ copy_all () {
 dl_src
 
 wfile="src/bbbio-set-sysconf" ; copy_all
+
+dl_usr_src
+
+wfile="arm32/usr/sbin/bb-growpart" ; copy_all
+wfile="arm32/usr/sbin/bb-regenerate-ssh-host-keys" ; copy_all
+wfile="arm32/usr/sbin/bb-resizefs" ; copy_all
+wfile="arm32/usr/sbin/bb-setup-mac-address" ; copy_all
+wfile="arm32/usr/sbin/bb-smsc95xx-set-mac-address" ; copy_all
