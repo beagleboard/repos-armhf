44289544df5fdbacd1893a26ed6b11e3e0c8f2c5
Robert Nelson <robertcnelson@gmail.com>
2026-04-30 12:49:37 -0500

    firmware-nonfree (20260410-1~bpo13+1)
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

---
+++ debian-bpo13-firmware-nonfree/version.sh
@@ -0,0 +1,16 @@
+#!/bin/bash -e
+
+mirror="https://deb.debian.org/debian"
+
+package_name="firmware-nonfree"
+debian_pkg_name="${package_name}"
+package_version="20260410"
+
+reprepro_dir="f/${package_name}"
+dl_path="pool/non-free-firmware/${reprepro_dir}/"
+
+debian_version="${package_version}-1~bpo13+1"
+
+dist="debian-trixie-armhf"
+suite="trixie"
+deb_arch="armhf"
