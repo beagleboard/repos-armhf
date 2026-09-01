546c0669a1ccfecda767554c06d4b3fc6b205504
Robert Nelson <robertcnelson@gmail.com>
2026-09-01 08:55:51 -0500

    bbb.io-kernel (1.20260901.0)
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- bbb.io-kernel/update.sh
+++ bbb.io-kernel/update.sh
@@ -321,8 +321,9 @@ do_some_bone () {
 	msg="6.19-bone" ; var="omap-psp" ; ver="V619X" ; current_kernel ; generate_kernel_mainline_bone
 	msg="7.0-bone"  ; var="omap-psp" ; ver="V70X"  ; current_kernel ; generate_kernel_mainline_bone
 	msg="7.1-bone"  ; var="omap-psp" ; ver="V71X"  ; current_kernel ; generate_kernel_mainline_bone
-
 	msg="7.2-bone"  ; var="omap-psp" ; ver="V72X"  ; current_kernel ; generate_kernel_mainline_bone
+
+	msg="7.3-bone"  ; var="omap-psp" ; ver="V73X"  ; current_kernel ; generate_kernel_mainline_bone
 }
 
 do_noble () {
